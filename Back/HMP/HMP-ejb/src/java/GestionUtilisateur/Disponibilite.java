0195c79ff1135b5ec13a774ee0997a70d321b9fe
AnaXaX <anas.boudyach@gmail.com>
2019-03-11 15:20:39 +0100

    Back
    mo7awalaaa fachila

---
+++ Back/HMP/HMP-ejb/src/java/GestionUtilisateur/Disponibilite.java
@@ -0,0 +1,53 @@
+package GestionUtilisateur;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Disponibilite implements Serializable {
+    @Column(nullable=true, unique=false)
+    private int test;
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Disponibilite)) {
+            return false;
+        }
+        Disponibilite other = (Disponibilite) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "GestionUtilisateur.Disponibilite[ id=" + id + " ]";
+    }
+    
+}
